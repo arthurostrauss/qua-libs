d16fb840003bd81ab5611815e3e8a41561b62e69
David StarBridge <ufocrew@gmail.com>
2024-11-05 11:02:01 +0800

    Update raw qua_config

--- Quantum-Control-Applications-QuAM/Superconducting/configuration/modify_quam.py
+++ Quantum-Control-Applications-QuAM/Superconducting/configuration/modify_quam.py
@@ -26,73 +26,75 @@ u = unit(coerce_to_integer=True)
 # Change active qubits
 # machine.active_qubit_names = ["q0"]
 
-for i in range(len(machine.qubits.items())):
-    machine.qubits[f"q{i+1}"].grid_location = f"{i},0"
+# for i in range(len(machine.qubits.items())):
+#     machine.qubits[f"q{i+1}"].grid_location = f"{i},0"
 
 # Update frequencies
-rr_freq = np.array([5932987219.0, 6023928729.0, 5866936123.0, 6079048431.0, 5971697831.0]) #* u.GHz
-rr_LO = 5.95 * u.GHz
-rr_if = rr_freq - rr_LO
-rr_max_power_dBm = -20
+# rr_freq = np.array([5932987219.0, 6023928729.0, 5866936123.0, 6079048431.0, 5971697831.0]) #* u.GHz
+# rr_LO = 5.95 * u.GHz
+# rr_if = rr_freq - rr_LO
+# rr_max_power_dBm = -20
 
-xy_freq = np.array([5108604110.9, 4834229255.6, 5146263353.0, 4674709204.1, 4880175329.7]) #* u.GHz
+# xy_freq = np.array([5108604110.9, 4834229255.6, 5146263353.0, 4674709204.1, 4880175329.7]) #* u.GHz
 # xy_LO = np.array([6.0, 6.5, 6.5, 7.0, 7.04]) * u.GHz
-xy_LO = np.array([4.9]*5) * u.GHz
-xy_if = xy_freq - xy_LO
-xy_max_power_dBm = -2
+# xy_LO = np.array([4.9]*5) * u.GHz
+# xy_if = xy_freq - xy_LO
+# xy_max_power_dBm = -2
 
 # NOTE: be aware of coupled ports for bands
-for i, q in enumerate(machine.qubits):
-    ## Update qubit rr freq and power
-    machine.qubits[q].resonator.opx_output.full_scale_power_dbm = rr_max_power_dBm
-    machine.qubits[q].resonator.opx_output.upconverter_frequency = rr_LO
-    machine.qubits[q].resonator.opx_input.downconverter_frequency = rr_LO
-    machine.qubits[q].resonator.opx_input.band = get_band(rr_LO)
-    machine.qubits[q].resonator.opx_output.band = get_band(rr_LO)
-    machine.qubits[q].resonator.intermediate_frequency = rr_if[i]
+# for i, q in enumerate(machine.qubits):
+#     ## Update qubit rr freq and power
+#     machine.qubits[q].resonator.opx_output.full_scale_power_dbm = rr_max_power_dBm
+#     machine.qubits[q].resonator.opx_output.upconverter_frequency = rr_LO
+#     machine.qubits[q].resonator.opx_input.downconverter_frequency = rr_LO
+#     machine.qubits[q].resonator.opx_input.band = get_band(rr_LO)
+#     machine.qubits[q].resonator.opx_output.band = get_band(rr_LO)
+#     machine.qubits[q].resonator.intermediate_frequency = rr_if[i]
 
-    ## Update qubit xy freq and power
-    machine.qubits[q].xy.opx_output.full_scale_power_dbm = xy_max_power_dBm
-    machine.qubits[q].xy.opx_output.upconverter_frequency = xy_LO[i]
-    machine.qubits[q].xy.opx_output.band = get_band(xy_LO[i])
-    machine.qubits[q].xy.intermediate_frequency = xy_if[i]
+#     ## Update qubit xy freq and power
+#     machine.qubits[q].xy.opx_output.full_scale_power_dbm = xy_max_power_dBm
+#     machine.qubits[q].xy.opx_output.upconverter_frequency = xy_LO[i]
+#     machine.qubits[q].xy.opx_output.band = get_band(xy_LO[i])
+#     machine.qubits[q].xy.intermediate_frequency = xy_if[i]
 
-    # Update flux channels
-    machine.qubits[q].z.opx_output.output_mode = "amplified"
-    machine.qubits[q].z.opx_output.upsampling_mode = "pulse"
+#     # Update flux channels
+#     machine.qubits[q].z.opx_output.output_mode = "amplified"
+#     machine.qubits[q].z.opx_output.upsampling_mode = "pulse"
 
-    ## Update pulses
-    # Readout
-    machine.qubits[q].resonator.operations["readout"].length = 2 * u.us
-    machine.qubits[q].resonator.operations["readout"].amplitude = 0.25
-    # Qubit saturation
-    machine.qubits[q].xy.operations["saturation"].length = 20 * u.us
-    machine.qubits[q].xy.operations["saturation"].amplitude = 0.5
-    # Single qubit gates - DragCosine
-    machine.qubits[q].xy.operations["x180_DragCosine"].length = 16
-    machine.qubits[q].xy.operations["x180_DragCosine"].amplitude = 0.8
-    machine.qubits[q].xy.operations["x90_DragCosine"].amplitude = (
-        machine.qubits[q].xy.operations["x180_DragCosine"].amplitude / 2
-    )
-    # Single qubit gates - Square
-    machine.qubits[q].xy.operations["x180_Square"].length = 40
-    machine.qubits[q].xy.operations["x180_Square"].amplitude = 0.1
-    machine.qubits[q].xy.operations["x90_Square"].amplitude = (
-        machine.qubits[q].xy.operations["x180_Square"].amplitude / 2
-    )
+#     ## Update pulses
+#     # Readout
+#     machine.qubits[q].resonator.operations["readout"].length = 2 * u.us
+#     machine.qubits[q].resonator.operations["readout"].amplitude = 0.25
+#     # Qubit saturation
+#     machine.qubits[q].xy.operations["saturation"].length = 20 * u.us
+#     machine.qubits[q].xy.operations["saturation"].amplitude = 0.5
+#     # Single qubit gates - DragCosine
+#     machine.qubits[q].xy.operations["x180_DragCosine"].length = 16
+#     machine.qubits[q].xy.operations["x180_DragCosine"].amplitude = 0.8
+#     machine.qubits[q].xy.operations["x90_DragCosine"].amplitude = (
+#         machine.qubits[q].xy.operations["x180_DragCosine"].amplitude / 2
+#     )
+#     # Single qubit gates - Square
+#     machine.qubits[q].xy.operations["x180_Square"].length = 40
+#     machine.qubits[q].xy.operations["x180_Square"].amplitude = 0.1
+#     machine.qubits[q].xy.operations["x90_Square"].amplitude = (
+#         machine.qubits[q].xy.operations["x180_Square"].amplitude / 2
+#     )
 
 # %%
-for qp in machine.qubit_pairs.values():
-    qp.coupler.opx_output.output_mode = "amplified"
-    qp.coupler.opx_output.upsampling_mode = "pulse"
+# for qp in machine.qubit_pairs.values():
+#     qp.coupler.opx_output.output_mode = "amplified"
+#     qp.coupler.opx_output.upsampling_mode = "pulse"
 
 # %%
 # save into state.json
-save_machine(machine, path)
+# save_machine(machine, path)
 
 # %%
 # View the corresponding "raw-QUA" config
-with open("qua_config.json", "w+") as f:
+with open("./Quantum-Control-Applications-QuAM/Superconducting/configuration/qua_config.json", "w+") as f:
     json.dump(machine.generate_config(), f, indent=4)
 
 # %%
+
+
